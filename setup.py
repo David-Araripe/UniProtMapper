f1c286921813dc3da852eb9fa3af80342b26b109
David-Araripe <david.araripe17@gmail.com>
2023-03-03 16:40:11 +0100

    including json in package required data

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup()
+    setuptools.setup(
+        name="UniProtMapper",
+        version="0.0.1-dev",
+        packages=["UniProtMapper"],
+        include_package_data=True,
+    )
