3395bdab7c0bb2a752413291f6ee8fefd6b72514
David-Araripe <david.araripe17@gmail.com>
2023-03-17 12:13:43 +0100

    update project setup to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-if __name__ == "__main__":
-    setuptools.setup(
-        name="UniProtMapper",
-        version="0.0.1-dev",
-        packages=["UniProtMapper"],
-        include_package_data=True,
-    )
